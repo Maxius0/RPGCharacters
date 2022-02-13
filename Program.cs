d9c99c4eba2fc8ea4a1220ea0780cd46a099ddbe
Marius Samson <mariusbsamson@gmail.com>
2022-02-13 23:29:47 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RPGCharacters
+{
+    /// <summary>
+    /// The main class, which only purpose is to instantiate the program.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Program execution starts here.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic RPG!");
+        }
+    }
+}
